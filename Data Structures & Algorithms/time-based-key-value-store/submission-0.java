599ed9b600105cb2cf8c49b6553f6bccd6917f4e
Shashank Shekhar <122527294+noddyStark@users.noreply.github.com>
2026-07-14 22:08:14 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.java
@@ -0,0 +1,69 @@
+
+// multiple values for the same key
+// can store different value for different timeStamp for the same key
+/*
+
+K -> V1 T1
+K -> V2 T2
+
+*/
+
+class Pair {
+    int timeStamp;
+    String value;
+
+    public Pair(int timeStamp, String value) {
+        this.timeStamp = timeStamp;
+        this.value = value;
+    }
+}
+
+class TimeMap {
+    HashMap<String, List<Pair>> mapOfKeyToValue;
+
+    public TimeMap() {
+        mapOfKeyToValue = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+
+        if (!mapOfKeyToValue.containsKey(key)) {
+            mapOfKeyToValue.put(key, new ArrayList<>());
+        } 
+        
+        mapOfKeyToValue.get(key).add(new Pair(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+
+        if (!mapOfKeyToValue.containsKey(key)) {
+            return "";
+        }
+        
+        List<Pair> existingPairs = mapOfKeyToValue.get(key);
+
+        int start = 0;
+        int end = existingPairs.size() - 1;
+
+        String result = "";
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            Pair currentPair = existingPairs.get(mid);
+
+            if (currentPair.timeStamp <= timestamp) {
+                // This is a valid answer, but there may be a later
+                // timestamp that is still <= the requested timestamp.
+                result = currentPair.value;
+                start = mid + 1;
+            } else {
+                // current timestamp is too large.
+                end = mid - 1;
+            }
+        }
+
+        return result;
+
+    }
+}
